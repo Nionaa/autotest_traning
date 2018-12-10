0cba185b14cec74915f6d03846a8fa472fb12725
Никитина Ольга Вадимовна <Nikitina.Olga5@corp.wildberries.ru>
2018-12-10 12:43:36 +0300

    Enter_with_iframe_pass

--- authorization.py
+++ authorization.py
@@ -7,26 +7,27 @@ wb = webdriver.Chrome()
 def open_page():
     wb.get("https://www.wildberries.ru/")
 
-
 def enter_page():
-    wb.find_element_by_link_text("Регистрация").click()
-    wb.find_element_by_class_name("auth-tabs-switch").click()
+    wb.find_element_by_link_text("Войти").click()
 
+def frame_switch():
+    wb.switch_to.frame(wb.find_element_by_tag_name("iframe"))
 
 def login_field():
     login = wb.find_element_by_css_selector("#Item_Login")
     login.clear()
     login.send_keys(data_storage.username)
 
-
 def password_field():
-    password = wb.find_element_by_name("Item.Password")
+    password = wb.find_element_by_css_selector("#Item_Password")
     password.clear()
     password.send_keys(data_storage.password1)
 
-
 def enter_button():
-    wb.find_element_by_class_name("auth-btn-login").click()
+    wb.find_element_by_id("signIn").click()
+
+def quit():
+    wb.quit()
 
 
 
